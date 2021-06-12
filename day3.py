0b2743f291877db7d0e6243c14ce40dffb1434fc
RONY-RAJ <85762080+RONY-RAJ@users.noreply.github.com>
2021-06-12 14:17:28 +0530

    Add files via upload
    age calculator and basic calculator

---
+++ day3.py
@@ -0,0 +1,43 @@
+#age calculator
+
+age=int(input("please enter your date of birth year="))
+print("you entered:",age)
+
+ctyr=int(input("please enter the current year="))
+print("you entered:",ctyr)
+
+result = ctyr-age
+print("your age is:",result)
+
+#calculator
+
+num1=int(input("please enter the first number:"))
+num2=int(input("please enter the second number:"))
+
+#addition
+result=num1+num2
+print("addition of two number is;",result)
+
+#subtraction
+result= num1-num2
+print("subtraction of numbers is:",result)
+
+#division
+result= num1/num2
+print("division of numbers is:",result)
+
+#multiplication
+result= num1*num2
+print("multiplication of numbers is:",result)
+
+#modulus
+result= num1%num2
+print("remainder of numbers is:",result)
+
+#floor division
+result= num1//num2
+print("quotient of numbers is:",result)
+
+#exponentiation
+result= num1**num2
+print("exponent of numbers is:",result)
