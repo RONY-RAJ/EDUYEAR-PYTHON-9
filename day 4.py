dbf03cf342eb1b68c4ac21da91799ca246628c96
RONY-RAJ <85762080+RONY-RAJ@users.noreply.github.com>
2021-06-15 00:06:03 +0530

    Add files via upload

---
+++ day 4.py
@@ -0,0 +1,22 @@
+#program for counting the number of y in the string given by the user, using string function count:
+
+print("this will count y in the given word")
+usr=str(input("please enter a word:"))
+occur= 'y'
+count= usr.count(occur)
+print("count of y is:",count)
+print()
+
+
+#swapping the string
+swap='EdUyEaR'
+print(swap.swapcase())
+print()
+
+
+#printing even indexed character
+ind='eduyear'
+ic= ind[::2]
+print(ic)
+
+
